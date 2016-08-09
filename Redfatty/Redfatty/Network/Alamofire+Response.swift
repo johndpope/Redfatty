686fb74b20574e9697958dfc84bddab20972ac81
redfatty <redfatty@163.com>
2016-08-09 23:01:11 +0800

    保存代码

---
+++ Redfatty/Redfatty/Network/Alamofire+Response.swift
@@ -0,0 +1,13 @@
+//
+//  Alamofire+Response.swift
+//  Redfatty
+//
+//  Created by huangjiong on 16/8/9.
+//  Copyright © 2016年 miwu. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import Protobuf
+
+//自定义响应序列化
