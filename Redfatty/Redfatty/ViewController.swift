98aece0d3c3c087bf7df33fa266f3b348a9b0003
jeson <jeson.huang@blemobi.com>
2016-08-09 10:10:07 +0800

    网络请求测试

--- Redfatty/Redfatty/ViewController.swift
+++ Redfatty/Redfatty/ViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //获取验证码
+        Alamofire.request(.GET, "http://192.168.1.245:8001/v1/login/authcode", parameters: ["phonecode":"+8613320946464","type":"1"]).responseData { response in
+
+            do {
+                let pmsg = try PMessage.parseFromData(response.data!)
+                do {
+                    
+                    let result =  try PResult.parseFromData(pmsg.data_p)
+                    print(result.errorCode)
+                    print(result.errorMsg)
+                    print(result.extraInfo)
+                    
+                } catch {
+                    
+                }
+                
+            } catch {
+                
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
