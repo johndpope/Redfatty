e9de864eafd2cf6f68461ce519138a35761d438f
jeson <jeson.huang@blemobi.com>
2016-08-09 09:40:56 +0800

    protobuf

---
+++ Redfatty/Redfatty/BridgeHeader.h
@@ -0,0 +1,33 @@
+//
+//  BridgeHeader.h
+//  Redfatty
+//
+//  Created by huangJiong on 16/8/9.
+//  Copyright © 2016年 miwu. All rights reserved.
+//
+
+#ifndef BridgeHeader_h
+#define BridgeHeader_h
+
+#pragma mark - protobuf
+
+#import "Account.pbobjc.h"
+#import "Chat.pbobjc.h"
+#import "Comment.pbobjc.h"
+#import "Community.pbobjc.h"
+#import "Contact.pbobjc.h"
+#import "Datapublishing.pbobjc.h"
+#import "Login.pbobjc.h"
+#import "Netdisk.pbobjc.h"
+#import "News.pbobjc.h"
+#import "Notification.pbobjc.h"
+#import "Oss.pbobjc.h"
+#import "Payment.pbobjc.h"
+#import "Platform.pbobjc.h"
+#import "Result.pbobjc.h"
+#import "Social.pbobjc.h"
+#import "Streaming.pbobjc.h"
+#import "Wallet.pbobjc.h"
+
+
+#endif /* BridgeHeader_h */
